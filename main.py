1dbe09d4bc7b32d62e4052ffa7db0dd8da4a04a2
Coder-RG <36698583+Coder-RG@users.noreply.github.com>
2021-12-15 16:27:31 +0530

    Starter code for environment

---
+++ main.py
@@ -0,0 +1,13 @@
+class Seven:
+    def __init__(self):
+        self.env = [0]*7
+        self.action_space = [-1,1]
+
+    def get_action(self, state):
+        return
+    
+    def step(self, action):
+        return
+
+    def __repr__(self):
+        return "This is an RL example that should be easy to understand."
